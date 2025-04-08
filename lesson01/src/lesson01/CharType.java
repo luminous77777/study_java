75b1356c5fae66cfb8f191e30ea354837326c467
luminous77777 <tiger2ys98@gmail.com>
2025-04-08 17:39:12 +0900

    자바 공부

---
+++ lesson01/src/lesson01/CharType.java
@@ -0,0 +1,21 @@
+package lesson01;
+
+public class CharType {
+	public static void main(String[] args) {
+		
+		char a = 'A';
+		System.out.println("a :"+ a);
+		
+		int b = a;
+		System.out.println("b : "+ b);
+		
+		char c = 66;
+		System.out.println("c : "+ c);
+		
+		int d = a+b;
+		System.out.println("d: " + d);
+	
+		String 한글변수 = "한글이름";
+		System.out.println("한글 이름 출력 :" + 한글변수);
+	}
+}
---
+++ lesson01/src/lesson01/FloatDoubleEx.java
@@ -0,0 +1,27 @@
+package lesson01;
+
+public class FloatDoubleEx {
+	public static void main(String[] args) {
+		
+		float myF = 0.12345678901234567890f;
+		double myD = 0.12345678901234567890;
+		
+		System.out.println(myF);
+		System.out.println(myD);
+		
+		
+		float f = 3.141592653589793f;
+		double d = 3.141592653589793d;
+		
+		System.out.println("float : "+ f);
+		System.out.println("double : " + d);
+		
+		double d2 = 3.141592653589793;
+		System.out.println("double : " + d2);
+		
+		System.out.println(3.14);
+		System.out.println(1);
+		System.out.println(3.14+1);
+		
+	}
+}
