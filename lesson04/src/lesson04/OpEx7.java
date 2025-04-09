d44080b01f60fffc7c625c71e7f8c1135fe70e1f
luminous77777 <tiger2ys98@gmail.com>
2025-04-09 12:59:42 +0900

    중간저장

--- lesson04/src/lesson04/OpEx7.java
+++ lesson04/src/lesson04/OpEx7.java
@@ -5,5 +5,9 @@ public class OpEx7 {
 		int score = 80;
 		String pass = score >60 ? "합격" : "불합격";
 		System.out.println(pass);
+		
+		String str = "1";
+		str += "문자추가\n";
+		System.out.println(str);
 	}
 }
