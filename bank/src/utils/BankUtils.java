07f591438299556359478a6e4d5215b621fec6f0
luminous77777 <tiger2ys98@gmail.com>
2025-04-25 18:22:53 +0900

    4월 25일 끝

---
+++ bank/src/utils/BankUtils.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.util.Scanner;
+
+public class BankUtils {
+	private static final Scanner scanner = new Scanner(System.in);// final : 상수
+	
+	public static String nextLine(String msg) { //메서드
+		System.out.println(msg);
+		return scanner.nextLine();
+	}
+	
+	public static int nextInt(String msg) {
+		return Integer.parseInt(nextLine(msg));
+	}
+	public static long nextLong(String msg) {
+		return Long.parseLong(nextLine(msg));
+	}
+	
+	public static boolean nextConfirm(String msg) {
+		String s = nextLine(msg);
+		return s.equalsIgnoreCase("y") || s.equalsIgnoreCase("yes");
+		
+	}
+
+}
